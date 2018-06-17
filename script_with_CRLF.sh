a99932d359014a46f477b4bf9d0a6d70ae48641b
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-06-18 00:07:07 +0100

    Update script with CRLF

--- script_with_CRLF.sh
+++ script_with_CRLF.sh
@@ -1,7 +1,2 @@
-#!/usr/bin/env bash
-
-echo Some more lines
-echo just to add some text
-echo this should be enough
-
-echo If you can see this line and the ones above means you have been able to execute this script
+echo "If you can see this message, it means that you do not have a CRLF at the end" |\
+   cat
